0ae67ca00e3c62fbc8bb42a79f05668a7c86d639
ingediego94 <inge.diego94@gmail.com>
2025-07-09 13:51:25 -0500

    se agrega table

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.getElementById('productForm').addEventListener('submit', async function
     const cantidad = document.getElementById('cantidad').value;
 
     const data = {
-        id: parseInt(id),
+        id: String(id),
         producto: producto,
         precio: parseFloat(precio),
         cantidad: parseInt(cantidad)
@@ -22,6 +22,9 @@ document.getElementById('productForm').addEventListener('submit', async function
         // ✅ Cleaning the form
         document.getElementById('productForm').reset();
 
+        // refreshing table content.
+        await getProduct();
+
     } catch (error) {
         console.info('Error al crear:', error);
     }
@@ -30,13 +33,50 @@ document.getElementById('productForm').addEventListener('submit', async function
 // GET method
 async function getProduct(){
     try{
-        const response = await fetch("http://localhost:3301/products");
+        const response = await fetch(url);
+
         if(!response.ok){
             throw new Error(`Error HTTP: ${response.status}`);
         }
         // console.log(resultado.status);   // confirma respuesta del servidor.
         const result = await response.json();
-        console.log("Lista de productos: ", result);
+        
+        // printing data on the table
+        const tableResults = document.querySelector("#showProductsTable tbody");
+
+        // result.forEach(product => {
+        //     tableResults.innerHTML += `
+        //     <tr>
+        //         <td>${product.id}</td>
+        //         <td>${product.producto}</td>
+        //         <td>${product.precio}</td>
+        //         <td>${product.cantidad}</td>
+        //     </tr>
+        //     `;
+        // });
+
+        //******************* */
+
+
+                if (Array.isArray(result)) {
+            let html = '';
+            result.forEach(product => {
+                html += `
+                <tr>
+                    <td>${product.id}</td>
+                    <td>${product.producto}</td>
+                    <td>${product.precio}</td>
+                    <td>${product.cantidad}</td>
+                </tr>
+                `;
+            });
+            tableResults.innerHTML = html;
+        } else {
+            throw new Error('La respuesta del servidor no es un arreglo válido.');
+        }
+
+
+
     } catch (error){
         console.info('Error al obtener los datos: ', error);
     }
@@ -47,9 +87,16 @@ async function getProduct(){
 
  // POST method
  async function addProduct(product) {
+    
+    // To validate before to send
+    if (!validateProduct(product)) {
+        console.log("Producto inválido. No se enviará al servidor.");
+        return; // exit the application if the data is incorrect
+    }
+    
     try{
             
-        const response = await fetch("http://localhost:3301/products", {
+        const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -72,16 +119,63 @@ async function getProduct(){
  }
 
 
-//  // PUT method
-//  async function updateProducts(productToUpdate) {
+function validateProduct(product){
+    if(!product.id || !product.producto || typeof product.precio !== "number" || !product.cantidad){
+        console.log("Se presentó un fallo.");
+        return false;
+    }
+    return true;
+}
+
+
+
+//  PUT method
+ async function updateProducts(productToUpdate) {
     
-//     const response = await fetch(`http://localhost:3301/products, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(response)
-//     })
+    try{
+        const response = await fetch(`${url}/${productToUpdate.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(productToUpdate)
+        });
+
+        if(!response.ok){
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("Producto actualizado: ", result);
+
+    } catch(error){
+        console.log('Error al actualizar');
+        
+    }
+
+ }
 
 
-//  }
+
+ // DELETE method
+ async function deleteProducts(id) {
+    try{
+        const response = fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        });
+
+        if(!response.ok){
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("Producto eliminado: ", result);
+    }catch(error){
+        throw new Error('Error al eliminar: ', error);
+    }
+    
+
+ }
+
+
+
